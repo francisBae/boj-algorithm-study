f095204e7a9c7a65a387b5af45b48690b9d0287b
francisBae <qowhdgn@gmail.com>
2021-09-15 18:34:39 +0900

    구간 합 구하기

---
+++ 11600~11699/11659.py
@@ -0,0 +1,17 @@
+#구간 합 구하기 4
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+
+arr = [0]*(N+1)
+line = list(map(int, sys.stdin.readline().split()))
+arr[1] = line[0]
+
+for i in range(2,N+1):
+    arr[i] = line[i-1]+arr[i-1] #이전 수를 더하기
+
+for i in range(0,M):
+    fNum, sNum = map(int, sys.stdin.readline().split())
+
+    print(f"{arr[sNum]-arr[fNum-1]}")
